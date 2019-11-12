f7c614cd33d38881ae424d85cf5fa31f7e6d9e05
Tong Guo <779222056@qq.com>
2019-11-12 13:14:55 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -14,12 +14,12 @@ using namespace std;
 string inPath = "./wiki50k/";
 
 extern "C"
-void setInPath(char *path) {
-    int len = strlen(path);
+void setInPath(char *path, int len) {
     inPath = "";
-    for (int i = 0; i < len; i++)
+    printf("init path...\n");
+    for (int i = 0; i < len; i++){
         inPath = inPath + path[i];
-    printf("Input Files Path : %s\n", inPath.c_str());
+    }
 }
 
 int relationTotal;
